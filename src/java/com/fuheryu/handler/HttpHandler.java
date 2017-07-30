748a0eaa5475ffa1df4f48921ae059e8aea0f455
funeyu <funeyu@gmail.com>
2017-07-30 19:44:20 +0800

    添加send response

--- src/java/com/fuheryu/handler/HttpHandler.java
+++ src/java/com/fuheryu/handler/HttpHandler.java
@@ -1,10 +1,17 @@
 package com.fuheryu.handler;
 
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.net.HttpRetryException;
 import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetEncoder;
+import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -16,8 +23,11 @@ public class HttpHandler implements Handler {
 
     private ExecutorService excutors = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
     private int count = 0;
+    private final static Charset charset = Charset.forName("UTF-8");
+    private final static CharsetEncoder encoder = charset.newEncoder();
 
     private HttpHandler(){}
+
 
     public void onRead(final SelectionKey selectionKey) {
 
@@ -41,7 +51,15 @@ public class HttpHandler implements Handler {
                     }
 
                     System.out.println(new String(bos.toByteArray()));
+
+
                     selectionKey.selector().wakeup();
+
+                    HTTPResponse response = new HTTPResponse();
+                    response.setContent("hello".getBytes());
+                    response.addDefaultHeaders();
+
+                    response.send(sc);
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -54,5 +72,53 @@ public class HttpHandler implements Handler {
     public static Handler createHander() {
         Handler h = new HttpHandler();
         return h;
+    }
+
+    private static class HTTPResponse {
+
+        private String version = "HTTP/1.1";
+        private int responseCode = 200;
+        private String responseReason = "ok";
+        private Map<String, String> headers = new LinkedHashMap<String, String>();
+        private byte[] content;
+
+        private HTTPResponse() {}
+
+        private void addDefaultHeaders() {
+
+            headers.put("Date", new Date().toString());
+            headers.put("Server", "NIO");
+            headers.put("connection", "close");
+            headers.put("Content-Length", Integer.toString(content.length));
+        }
+
+        private void writeLine(String line, SocketChannel channel) throws IOException {
+            channel.write(encoder.encode(CharBuffer.wrap(line + "\r\n")));
+        }
+
+        private void setContent(byte[] content) {
+
+            this.content = content;
+        }
+
+        private void send(SocketChannel channel) {
+
+            try {
+                writeLine(version + " " + responseCode + " " + responseReason, channel);
+
+                for(Map.Entry<String, String> header : headers.entrySet()) {
+                    writeLine(header.getKey() + ": " + header.getValue(), channel);
+                }
+                writeLine("", channel);
+
+                channel.write(ByteBuffer.wrap(content));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static class HTTPRequest {
+
     }
 }
